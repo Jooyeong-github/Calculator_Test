0eb97abc96235c849163f6ee1b5cbea437f06b85
Jooyeong <supershicker@gmail.com>
2023-11-30 19:02:14 +0900

    lv1

---
+++ Calculator/main.swift
@@ -0,0 +1,20 @@
+let calculator = Calculator()
+
+// Lv1 연산
+// 덧셈 연산
+let addResult = calculator.calculateLv1(operator: "+", firstNumber: 5.0, secondNumber: 3.0)
+
+// 뺄셈 연산
+let subtractResult = calculator.calculateLv1(operator: "-", firstNumber: 5.0, secondNumber: 3.0)
+
+// 곱셈 연산
+let multiplyResult = calculator.calculateLv1(operator: "*", firstNumber: 5.0, secondNumber: 3.0)
+
+// 나눗셈 연산
+let divideResult = calculator.calculateLv1(operator: "/", firstNumber: 5.0, secondNumber: 3.0)
+
+print("//////// Lv1 ////////")
+print("addResult : \(addResult)")
+print("subtractResult : \(subtractResult)")
+print("multiplyResult : \(multiplyResult)")
+print("divideResult : \(divideResult)")
