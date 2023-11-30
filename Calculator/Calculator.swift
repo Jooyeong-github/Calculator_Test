a4ea06ba27b67f8c075e180c2283b029c7b8c1f1
Jooyeong <supershicker@gmail.com>
2023-11-30 19:09:16 +0900

    lv1

--- Calculator/Calculator.swift
+++ Calculator/Calculator.swift
@@ -1,4 +1,5 @@
 class Calculator {
+    // Lv1
     func calculateLv1(operator op: String, firstNumber: Double, secondNumber: Double) -> Double {
         switch op {
         case "+":
@@ -13,6 +14,27 @@ class Calculator {
             } else {
                 return 0 // 0으로 나누는 경우 0을 리턴
             }
+        default:
+            return 0 // 정확한 연산자가 전달되지 않으면 0을 리턴
+        }
+    }
+    
+    // Lv2
+    func calculateLv2(operator op: String, firstNumber: Double, secondNumber: Double) -> Double {
+        switch op {
+        case "+":
+            return firstNumber + secondNumber // 덧셈
+        case "-":
+            return firstNumber - secondNumber // 뺄셈
+        case "*":
+            return firstNumber * secondNumber // 곱셈
+        case "/":
+            if secondNumber != 0 {
+                return firstNumber / secondNumber // 나눗셈 (0으로 나누지 않음)
+            } else {
+                return 0 // 0으로 나누는 경우 0을 리턴
+            }
+        case "%":
         default:
             return 0 // 정확한 연산자가 전달되지 않으면 0을 리턴
         }
--- Calculator/main.swift
+++ Calculator/main.swift
@@ -1,6 +1,7 @@
 let calculator = Calculator()
 
-// Lv1 연산
+// ******** Lv1 연산 ********
+
 // 덧셈 연산
 let addResult = calculator.calculateLv1(operator: "+", firstNumber: 5.0, secondNumber: 3.0)
 
@@ -13,8 +14,11 @@ let multiplyResult = calculator.calculateLv1(operator: "*", firstNumber: 5.0, se
 // 나눗셈 연산
 let divideResult = calculator.calculateLv1(operator: "/", firstNumber: 5.0, secondNumber: 3.0)
 
-print("//////// Lv1 ////////")
+print("******** Lv1 ********")
 print("addResult : \(addResult)")
 print("subtractResult : \(subtractResult)")
 print("multiplyResult : \(multiplyResult)")
 print("divideResult : \(divideResult)")
+
+
+// ******** Lv2 연산 ********
